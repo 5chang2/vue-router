e55acb5ab82470b08d2b2de0c7ce31ec75577998
change <och8808@gmail.com>
2020-05-26 04:36:47 +0900

    update

---
+++ src/views/Lotto.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Lotto</h1>
+        <button @click="creatNumber">숫자생성</button>
+        <p>{{ lucky }}</p>
+    </div>
+</template>
+
+<script>
+var _ = require('lodash');
+export default {
+    data: function(){
+        return {
+            numbers: _.range(1,46),
+            lucky: '',
+        }
+    },
+    methods: {
+        creatNumber(){
+            this.lucky = _.sampleSize(this.numbers, 6)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Lunch.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Lunch</h1>
+        <p>{{ menus.join(',') }}</p>
+        <!-- <li v-for="menu in menus" :key="menu">{{ menu }}</li> -->
+        <button @click="pick">랜덤선택</button>
+
+        <p>{{ choice }}</p>
+    </div>
+</template>
+
+<script>
+var _ = require('lodash');
+export default {
+    data: function(){
+        return {
+            menus: ['짜장', '짬뽕', '김밥', '라면'],
+            choice: '',
+        }
+    },
+    methods: {
+        pick(){
+            this.choice = _.sample(this.menus)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
